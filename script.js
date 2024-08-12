7cfedc74b9ed394fc8298a40de02314bc4043737
Md. Al Mamun <almamunprojapur111@gmail.com>
2024-08-13 02:27:28 +0600

    sending my signin page with js file

---
+++ script.js
@@ -0,0 +1,22 @@
+// Click here for jump to the home page//
+// Click here for jump to the home page//
+
+const comfortyHome = document.getElementById('comfortyLink');
+
+        const jumpLink = 'index.html'; 
+
+        comfortyHome.addEventListener('click', () => {
+            window.location.href = jumpLink;
+        });
+
+
+// Click here for jump to the signin page//
+// Click here for jump to the signin page//
+
+const comfortyReg = document.getElementById('signinLink');
+
+    const formLink = 'signin.html';
+
+    comfortyReg.addEventListener('click', () => {
+            window.location.href = formLink;
+        });
